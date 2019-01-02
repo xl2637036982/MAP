782e952f34354234dd6a11b60db7d3f52a4783e5
xl2637036982 <2637036982@qq.com>
2019-01-02 13:35:32 +0800

    侧拉

--- app/src/main/java/com/example/administrator/map/MainActivity.java
+++ app/src/main/java/com/example/administrator/map/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.administrator.map;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,32 +31,77 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
+import com.google.gson.Gson;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    private Song song;
 private MapView mapView;
 private LocationClient mLocationClient;
 private BaiduMap baiduMap;
 private boolean isFirstLocate = true;
 private TextView textView;
+private ImageButton btn_mp3;
+private    String url;
+private  String name_song;
+    private MediaPlayer mediaPlayer;
+    private  boolean a = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+
+
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.bzqll.com/music/netease/songList?key=579621905&id=3778678&limit=100&offset=0")
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String s = response.body().string();
+                Gson gson = new Gson();
+                song=gson.fromJson(s,Song.class);
+                if (song!=null&&song.getData()!=null){
+                    url = song.getData().getSongs().get((int) (Math.random()*100)).getUrl();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Uri uri1 = Uri.parse(url);
+                            mediaPlayer = MediaPlayer.create(MainActivity.this,uri1);
+                        }
+                    });
+                }
+            }
+        });
+
+        btn_mp3 = findViewById(R.id.play);
          textView= (TextView) findViewById(R.id.text);
         mapView = (MapView) findViewById(R.id.map);
         baiduMap = mapView.getMap();
         baiduMap.setMyLocationEnabled(true);
         List<String> permissionList = new ArrayList<>();
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
-        }
+
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(Manifest.permission.READ_PHONE_STATE);
@@ -64,6 +116,28 @@ private TextView textView;
         } else {
             requestLocation();
         }
+
+        btn_mp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                        if (a){
+                            Toast.makeText(MainActivity.this,"正在播放音乐",Toast.LENGTH_SHORT).show();
+                            btn_mp3.setImageResource(R.mipmap.btn_pause);
+                            mediaPlayer.start();
+                        }else {
+                            btn_mp3.setImageResource(R.mipmap.btn_play);
+                            mediaPlayer.pause();
+                        }
+                        if (a){
+                            a=false;
+                        }else {
+                            a=true;
+                        }
+                    }
+                });
+
+
     }
 
     private void requestLocation() {
@@ -124,26 +198,7 @@ private TextView textView;
             if (location.getLocType() == BDLocation.TypeGpsLocation || location.getLocType() == BDLocation.TypeNetWorkLocation) {
                 navigateTo(location);
             }
-//            currentPosition = new StringBuilder();
-//            currentPosition.append("纬度:").append(location.getLatitude()).append("\n");
-//            currentPosition.append("经度:").append(location.getLongitude()).append("\n");
-//            currentPosition.append("国家:").append(location.getCountry()).append("\n");
-//            currentPosition.append("省:").append(location.getProvince()).append("\n");
-//            currentPosition.append("市:").append(location.getCity()).append("\n");
-//            currentPosition.append("区:").append(location.getDistrict()).append("\n");
-//            currentPosition.append("街道:").append(location.getStreet()).append("\n");
-//            currentPosition.append("定位方式：");
-//            if (location.getLocType() == BDLocation.TypeGpsLocation) {
-//                currentPosition.append("GPS");
-//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
-//                currentPosition.append("网络");
-//            }
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    positionText.setText(currentPosition);
-//                }
-//            });
+
         }
 
 
